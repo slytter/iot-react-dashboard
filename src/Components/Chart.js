cc27ed7112851304d18d36e2d784ba60f34873ba
Nikolaj <nikolaj.sn@hotmail.com>
2020-04-28 02:24:23 +0200

    Add componentDidmount to chart

--- src/Components/Chart.js
+++ src/Components/Chart.js
@@ -90,7 +90,7 @@ export default class Chart extends Component {
 	
 	componentDidMount() {
 		console.log({DIDMOUNT: this.state.data})
-		//this.updateStateWithData()
+		this.updateStateWithData()
 	}
 
 	
